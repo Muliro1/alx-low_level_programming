0032f997ef7a0192d8c6e310bfd9e33e69dac025
muliro <mulirokhaemba@gmail.com>
2023-03-17 18:43:29 +0300

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -33,6 +33,6 @@ int main(void)
 		else if (mine == 0)
 			printf("Last digit of %d is %d and is 0\n", n, mine);
 		else
-			printf("Last digit of %d is %d and is less than 6 and not zero\n", n, mine);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, mine);
 		return (0);
 }
