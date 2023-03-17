31c9875fab8c1c8310135764d8b69bbdfc9762c9
muliro <mulirokhaemba@gmail.com>
2023-03-18 01:02:15 +0300

    advance

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+
+
+
+
+
+
+
+int main(void)
+{
+	int i, s;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (s = i + 1; s <= '9'; s++)
+		{
+			if (s != i)
+			{
+				putchar(i);
+				putchar(s);
+				if (i == '8' && s == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
