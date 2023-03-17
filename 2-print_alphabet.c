297501b59173020982b508545d988a4feb2d8409
muliro <mulirokhaemba@gmail.com>
2023-03-17 22:03:14 +0300

    alphabet

---
+++ 2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ * Description: Prints alphabet
+ */
+
+
+
+
+
+
+
+
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a', l <= 'z', l++)
+		putchar(l);
+		putchar('\n');
+		return (0);
+}
