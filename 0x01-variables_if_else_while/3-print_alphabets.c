56ccbb68973d1a433a77cd1ae71cc59df02a1f48
muliro <mulirokhaemba@gmail.com>
2023-03-17 22:39:03 +0300

    alphabet 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+
+
+
+
+
+
+
+
+
+
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+		putchar(lower_case);
+	for (lower_case = 'A'; lower_case <= 'Z'; lower_case++)
+		putchar(lower_case);
+
+	return (0);
+}
