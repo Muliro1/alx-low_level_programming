b1b8d492a50fe7ee2d3027bf15ab0c7043e30193
muliro <mulirokhaemba@gmail.com>
2023-03-22 03:47:59 +0300

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * print_alphabet_x10 - prints ten times.
  * Return: void
  */
@@ -23,8 +23,8 @@ void print_alphabet_x10(void)
 		{
 			_putchar(c);
 			_putchar('\n');
-			i++;
 		}
+		i++;
 	}
 }
 
