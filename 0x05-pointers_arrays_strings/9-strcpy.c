a0071b23004961477742601aee1da903c6ff2295
muliro <mulirokhaemba@gmail.com>
2023-03-27 19:28:48 +0300

    strcopy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+
+
+
+
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int x = 0;
+
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	for (; x < i; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
