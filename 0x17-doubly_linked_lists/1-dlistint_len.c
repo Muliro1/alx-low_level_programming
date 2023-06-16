775c593c8edb80922843c35af236768f284a5eaf
Muliro1 <mulirokhaemba@gmail.com>
2023-06-16 03:55:47 +0300

    len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ *  listint_len - returns the number of elements
+ *  @h: linked list of type listint_t to traverse
+ *  Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t number = 0;
+
+	while (h)
+	{
+		number++;
+		h = h->next;
+	}
+	return (number);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -13,5 +13,6 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 struct dlistint_s;
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
