c0263e46b441c24634cecb36650bc28f0b279403
Muliro1 <mulirokhaemba@gmail.com>
2023-06-16 03:58:39 +0300

    lenth

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stddef.h>
 /**
- *  listint_len - returns the number of elements
+ *  dlistint_len - returns the number of elements
  *  @h: linked list of type listint_t to traverse
  *  Return: number of nodes
  */
