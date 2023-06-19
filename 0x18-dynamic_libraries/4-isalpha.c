257f0b67654a16fd2da42fe7d34fcb1d88c9ec42
Muliro1 <mulirokhaemba@gmail.com>
2023-06-19 09:34:08 +0300

    task 0

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alpha
+ * @c: to be checked
+ * Return: 1 for alpha and 0 for nothing else.
+ */
+
+
+
+
+
+
+
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 98) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
