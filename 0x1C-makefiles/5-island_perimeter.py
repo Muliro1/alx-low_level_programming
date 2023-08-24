fbb04a87dc6af96f1a979fd087d3363984a8aaf0
Muliro <mulirokhaemba@gmail.com>
2023-08-24 21:44:03 +0300

    fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,13 +15,13 @@ def island_perimeter(grid):
     edges = 0
     size = 0
 
-    for i in range(height):
+    for a in range(height):
         for j in range(width):
-            if grid[i][j] == 1:
+            if grid[a][j] == 1:
                 size += 1
                 ''' checking for boundery or a lake '''
-                if (j > 0 and grid[i][j - 1] == 1):
+                if (j > 0 and grid[a][j - 1] == 1):
                     edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
+                if (a > 0 and grid[a - 1][j] == 1):
                     edges += 1
     return size * 4 - edges * 2
